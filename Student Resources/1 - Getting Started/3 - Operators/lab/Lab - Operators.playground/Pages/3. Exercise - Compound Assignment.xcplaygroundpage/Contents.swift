03ff5c58ba22cfcbba7db6155ce0c849a6c1c1ca
Mariham Wasfy <66705756+marihamWasfy@users.noreply.github.com>
2020-06-10 03:46:32 +0200

    Update Contents.swift

--- Student Resources/1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/3. Exercise - Compound Assignment.xcplaygroundpage/Contents.swift
+++ Student Resources/1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/3. Exercise - Compound Assignment.xcplaygroundpage/Contents.swift
@@ -3,6 +3,12 @@
  
  Declare a variable whose value begins at 10. Using addition, update the value to 15 using the compound assignment operator. Using multiplication, update the value to 30 using compound assignment. Print out the variable's value after each assignment.
  */
+var value = 10
+print(value)
+value += 5
+print(value)
+value *= 2
+print(value)
 
 
 /*:
@@ -16,7 +22,18 @@
  
  Print the balance of your piggy bank after each step.
  */
-
+var piggyBank = 0
+print(piggyBank)
+piggyBank += 10
+print(piggyBank)
+piggyBank += 20
+print(piggyBank)
+piggyBank /= 2
+print(piggyBank)
+piggyBank *= 3
+print(piggyBank)
+piggyBank -= 3
+print(piggyBank)
 
 
 
