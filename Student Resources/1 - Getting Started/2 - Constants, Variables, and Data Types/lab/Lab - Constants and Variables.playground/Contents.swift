78e6cda633caff10b3bbd78837122a9aec8915e1
minarelaasser <33254588+minarelaasser@users.noreply.github.com>
2020-06-10 03:13:15 +0200

    Update Contents.swift

--- Student Resources/1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Contents.swift
+++ Student Resources/1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Contents.swift
@@ -1,50 +1,16 @@
 /*:
- ## Exercise - Constants
+ ## App Exercise - Step Goal
  
- Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
+ >These exercises reinforce Swift concepts in the context of a fitness tracking app.
+ Your fitness tracking app needs to know goal number of steps per day. Create a constant `goalSteps` and set it to 10000.
  */
-
+let goalSteps = 1000
 
 /*:
- Now assume you go through and remove a lot of your friends that aren't active on social media. Update your `friends` constant to a lower number than it currently is between 1 and 900.
+ Use two `print` functions to print two separate lines to the console. The first line should say "Your step goal for the day is:", and the second line should print the value of `goalSteps` by referencing your constant.
  */
+ print ("You step goal for the day is:")
+ print (goalSteps)
 
 
-/*:
- Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friend` constant to a lower number so that the playground will compile properly.
- */
-
-
-/*:
- Declare a variable `age` and set it to your own age. Print `age` to the console.
- */
-
-
-/*:
- Now pretend you just had a birthday, and update the `age` variable accordingly. Print `age` to the console.
- */
-
-/*:
- 
- */
-
-
-/*:
- Create a double variable with a value of 1.1. Update it to 2.2, 3.3, and 4.4. Print out the value after each assignment (again by referencing the variable you created).
- */
-
-
-/*:
- Create a Boolean variable and set it to `true`. Print the variable, then assign it a value of `false`, and print it again.
- */
-
-
-/*:
- Create two variables: one with a value of 0, the other with a value of 0.0. Try to assign the second variable to the first, and you'll receive an error. Add the necessary type annotation to allow the second variable to be assigned to the first.
- */
-
-
-/*:
- Create a variable integer with a value of 1,000,000,000. Format it using commas, so it's easier to read.
- */
-
+//: [Previous](@previous)  |  page 2 of 10  |  [Next: Exercise - Variables](@next)
