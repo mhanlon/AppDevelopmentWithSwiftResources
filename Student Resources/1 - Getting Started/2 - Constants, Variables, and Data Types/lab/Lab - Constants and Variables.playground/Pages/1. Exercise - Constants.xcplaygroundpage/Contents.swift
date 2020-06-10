2cd0e912c24384c0dc8f85e36bd6f8477b77cd99
MaggieShammaa <64765124+MaggieShammaa@users.noreply.github.com>
2020-06-10 03:56:07 +0200

    Update Contents.swift

--- Student Resources/1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ Student Resources/1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -1,10 +1,11 @@
 /*:
  ## Exercise - Constants
-
  Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
- */
-
-
+ */let friends=755
+   print(friends)
+//friends=700
+//print(friends)
+print("The code cannot compile because a constant cannot be updated")
 /*:
  Now assume you go through and remove friends that aren't active on social media. Attempt to update your `friends` constant to a lower number than it currently is. Observe what happens and then move to the next step.
  */
@@ -15,4 +16,4 @@
  */
 
 
-//: page 1 of 10  |  [Next: App Exercise - Step Goal](@next)
+//: page 1 of 10  |  [Next: App Exercise - Step Goal](@next)"
