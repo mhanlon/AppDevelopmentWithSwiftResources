5c7606051d7b76dd4928682a57838bf637fefc1e
Mariham Wasfy <66705756+marihamWasfy@users.noreply.github.com>
2020-06-10 04:44:57 +0200

    Update Contents.swift

--- Student Resources/1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Contents.swift
+++ Student Resources/1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Contents.swift
@@ -11,27 +11,36 @@
 /*:
  Create a `perimeter` constant whose value equals `width` plus `width` plus `height` plus `height`. Print out the result.
  */
+let width = 100
+let height = 250
+let area = width * height
+print(area)
 
+let perimeter = width + width + height + height
+print(perimeter)
 
 /*:
  Print out what you think 10 + 2 * 5 evaluates to. Then print out the actual expression (i.e., `print(10 + 2 * 5)`)
  */
-
+print(20)
+print(10 + 2 * 5)
 
 /*:
  In a separate statement, add in the necessary parentheses so that addition takes place before multiplication.
  */
-
+print((10 + 2) * 5)
 
 /*:
  Create a constant, `divisionResult`, that's the result of 10 divided by 3. Print the constant's value.
  */
-
+let divisionResult = 10 / 3
+print(divisionResult)
 
 /*:
  Create a constant, `moreAccurateResult`, that's also the result of 10 divided by 3, but includes the repeating decimal. Print this value.
  */
-
+let moreAccurateResult: Double = 10 / 3
+print(moreAccurateResult)
 
 /*:
  Given the value pi (3.1415927), create a `radius` constant with a value of 5.0. Use the following equations to calculate the diameter and circumference of a circle, and print the results:
@@ -40,19 +49,34 @@
  
  *circumference = 2 * pi * radius.*
  */
-
+let pi = 3.1415927
+let radius = 5.0
+let diameter = 2 * radius
+print(diameter)
+let circumference = 2 * pi * radius
+print(circumference)
 
 /*:
  Declare a variable whose value begins at 10. Using addition and the compound assignment operator, update the value to 15. Using multiplication and compound assignment, update the value to 30. Print out the variable's value after each assignment.
  */
+var value = 10
+print(value)
+value += 5
+print(value)
+value *= 2
+print(value)
 
 
 /*:
  Create an integer constant with a value of 10, and a double constant with a value of 3.2. Cast the `Double` to an `Int`, then multiply it by the integer constant. Print out the resulting value.
  */
-
+let value1 = 10
+let value2 = 3.2
+let result = Int(value2) * value1
+print(result)
 
 /*:
  Create an integer constant. Using the modulus operator, set its value to the remainder of 12 divided by 5.
  */
-
+let value3: Int = 12 % 5
+print(value3) 
