8a7ec8ea9b64e28526ce2fe2c3d6e8346bbeb88a
MaggieShammaa <64765124+MaggieShammaa@users.noreply.github.com>
2020-06-10 04:29:27 +0200

    Update Contents.swift

--- Student Resources/1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Contents.swift
+++ Student Resources/1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Contents.swift
@@ -3,26 +3,31 @@
  
  Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
  */
-
+let friends=755
 
 /*:
  Now assume you go through and remove a lot of your friends that aren't active on social media. Update your `friends` constant to a lower number than it currently is between 1 and 900.
  */
 
+//friends=700
+
 
 /*:
  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friend` constant to a lower number so that the playground will compile properly.
  */
-
+print("constants are fixed values")
 
 /*:
  Declare a variable `age` and set it to your own age. Print `age` to the console.
  */
-
+var age=27
+print(age)
 
 /*:
  Now pretend you just had a birthday, and update the `age` variable accordingly. Print `age` to the console.
  */
+ age=28
+print(age)
 
 /*:
  
@@ -32,19 +37,37 @@
 /*:
  Create a double variable with a value of 1.1. Update it to 2.2, 3.3, and 4.4. Print out the value after each assignment (again by referencing the variable you created).
  */
+var value=1.1
+print(value)
+value=2.2
+print(value)
+value=3.3
+print(value)
+value=4.4
+//print(value)
 
 
 /*:
  Create a Boolean variable and set it to `true`. Print the variable, then assign it a value of `false`, and print it again.
  */
-
+var trueORFalse=true
+print(trueORFalse)
+trueORFalse=false
+print(trueORFalse)
 
 /*:
  Create two variables: one with a value of 0, the other with a value of 0.0. Try to assign the second variable to the first, and you'll receive an error. Add the necessary type annotation to allow the second variable to be assigned to the first.
  */
-
+var decimalValue: Float
+decimalValue=0
+print(decimalValue)
+decimalValue=0.0
+print(decimalValue)
 
 /*:
  Create a variable integer with a value of 1,000,000,000. Format it using commas, so it's easier to read.
  */
-
+ var integerValue:Int
+integerValue=1_000_000_000
+ 
+ 
