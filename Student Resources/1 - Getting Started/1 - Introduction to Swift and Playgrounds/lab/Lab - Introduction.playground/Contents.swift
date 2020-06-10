0c8cd4254cd54e9d55bb86385f10c3483e115b5a
Mariham Wasfy <66705756+marihamWasfy@users.noreply.github.com>
2020-06-10 03:10:47 +0200

    Update Contents.swift

--- Student Resources/1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Contents.swift
+++ Student Resources/1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Contents.swift
@@ -1,22 +1,28 @@
-/*: 
- ## Exercise: Use Playgrounds
- 
+import Foundation
+/*:
+ ## Exercise - Use Playgrounds
  The code below prints a few short statements about what you have learned in this lesson. Open the console area and view the code's output.
  */
-
 print("I have learned the following:")
 print("What features make Swift a modern and safe language")
 print("How to use the Swift REPL in Terminal")
 print("How to use playgrounds to make writing Swift fun and simple")
- 
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
 
+print ("I love it when you call me senorita")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
+
+print ("I wish I could pretend I didn't need ya")
+print ("But every touch is ooh la la la")
+print ("It's true, la la la")
+print ("Ooh, I should be running")
+print ("Ooh, you keep me coming for ya")
+
 
 
 //:page 14 of 16  |  [Next: Exercise: Go! Fight! Win!](@next)
