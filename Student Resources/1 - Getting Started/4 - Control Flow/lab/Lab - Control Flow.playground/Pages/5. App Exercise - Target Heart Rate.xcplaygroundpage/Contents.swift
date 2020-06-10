25ee444bc7b04e60c394c16178da2dafc843a798
MaggieShammaa <64765124+MaggieShammaa@users.noreply.github.com>
2020-06-10 04:23:49 +0200

    Update Contents.swift

--- Student Resources/1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
+++ Student Resources/1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
@@ -11,5 +11,16 @@ let targetLowerBound = 120
 let targetUpperBound = 150
 let currentHR = 147
 
+let isInTarget=(currentHR >= 120 && currentHR <= 150)
+let isBelowTarget=(currentHR < 120)
+let isAboveTarget=(currentHR > 150)
+
+if (isInTarget){
+    print("You're right on track!")}
+    else if(isBelowTarget){
+        print("You're doing great, but try to push it a bit!")}
+        else if(isAboveTarget){
+            print("You're on fire! Slow it down just a bit.")}
+
 
 //: [Previous](@previous)  |  page 5 of 9  |  [Next: Exercise - Switch Statements](@next)
