6688a6c536c08e739b5fdfccf007a1226426ff35
minarelaasser <33254588+minarelaasser@users.noreply.github.com>
2020-06-10 06:39:43 +0200

    Update Contents.swift

--- Student Resources/1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
+++ Student Resources/1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
@@ -6,11 +6,14 @@
 let number1 = 14
 let number2 = 25
 
-var largest: Int
-if number1 > number2 {
-    largest = number1
-} else {
-    largest = number2
-}
+var largest: Int = 0 
+// if number1 > number2 {
+//     largest = number1
+// } else {
+//     largest = number2
+// }
+
+(number1 > number2) ? (largest = number1) : (largest = number1)
+print("The largest number is: ",largest)
 
 //: [Previous](@previous)  |  page 8 of 9  |  [Next: App Exercise - Ternary Messages](@next)
