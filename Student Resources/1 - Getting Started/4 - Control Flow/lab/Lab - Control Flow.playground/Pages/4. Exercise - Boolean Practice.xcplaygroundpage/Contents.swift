8f3ceed93a5d245797655ea3eb774fee973e8367
YasminAMassoud <55138409+YasminAMassoud@users.noreply.github.com>
2020-06-19 14:51:33 +0200

    Update Contents.swift

--- Student Resources/1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/4. Exercise - Boolean Practice.xcplaygroundpage/Contents.swift
+++ Student Resources/1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/4. Exercise - Boolean Practice.xcplaygroundpage/Contents.swift
@@ -9,10 +9,21 @@
  Another friend brings up a restaurant she thinks will fit both of your criteria. This restaurant's attributes are represented by a few constants below. Write an if-else statement that will print "Let's go!" if the restaurant's attributes match the group's dietary requirements, and otherwise will print "Sorry, we'll have to think of somewhere else."
  */
 
-let hasFish = true
-let hasPizza = false
-let hasVegan = true
+let hasFish : Bool
+let hasPizza : Bool
+let hasVegan  : Bool
 
+hasFish = true 
+hasPizza = true 
+hasVegan = true
+
+if ((hasVegan == true ) && ( (hasPizza || hasFish) == true ) )
+{ 
+print("continue ") } 
+else 
+{ 
+print("we need somehwere else") 
+}
 
 /*:
  Imagine you're trying to decide whether or not to go on a walk. You decide that you'll go on a walk if it's not raining or if it's 82 degress or warmer and sunny out. Create a constant `isNiceWeather` that is equal to an expression that evaluates to a boolean indicating whether or not the weather is nice enough for you to go for a walk. Write an if statement that will print "I'm going for a walk!" if the weather is nice.
