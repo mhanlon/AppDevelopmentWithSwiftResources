c2bb0c466ab98e1b1cd358bca0d4473e6f68c83b
YasminAMassoud <55138409+YasminAMassoud@users.noreply.github.com>
2020-06-19 06:22:39 +0200

    Update Contents.swift

--- Student Resources/1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ Student Resources/1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -3,21 +3,28 @@
  
  Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
  */
-
+var firstDecimal = 0.0
+var secondDecimal = 0.1 
 
 /*:
  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
-
+var trueOrFalse = true 
+//firstDecimal = trueOrFalse
+print("cant assign boolean to double")
 
 /*:
  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
 
-
+var trueOrFalse2 = "true" 
+//firstDecimal = trueOrFalse2
+print("cant assign string to double")
 /*:
  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
-
+var trueOrFalse3 = 3
+//firstDecimal = trueOrFalse3
+print("cant assign int to double")
 
 //: [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
